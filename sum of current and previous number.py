9c2cbe1639eb9310ab4325515c45ac9f339449c5
Shai_Base <shaibase1328@gmail.com>
2024-01-27 13:27:39 +0800

    made iterations

--- sum of current and previous number.py
+++ sum of current and previous number.py
@@ -4,4 +4,10 @@
 # starting number
 previous_number = 0
 
+# iterations
+for i in range(1,11):
+    sum = previous_number + i
+    print('current number ',i , 'previous number ',previous_number ,'sum:', sum)
+    previous_number = i
+
 
